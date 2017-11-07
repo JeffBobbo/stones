237f41e1f80ec616777adc11c316a601436c8dfd
JeffBobbo <james.delph@hotmail.co.uk>
2017-11-07 13:42:52 +0000

    um

--- stones.pl
+++ stones.pl
@@ -18,6 +18,9 @@ print_state([P|S]) :-
   nl,
   print_state(S).
 
+list_popfront([X|Tail], X, Tail).
+list_pushfront(X, L, [X|L]).
+
 /**
  * Question 1
  * Produce all possible states, S2, from the current state, S1
@@ -31,9 +34,34 @@ print_state([P|S]) :-
  *     [3, 1], % second and third heap (1)
  *   ]
  */
+rempve([F|O], O, N):-
+  N = F.
+remove([F|O], [NF|O], N) :-
+  N < F,
+  NF is F-N.
+remove([F|O], [F|NO], N) :-
+  remove(O, NO, N).
 
-take([P|S], N, St) :-
-  P1 is P-N,
-  P1 > 0,
-  St = [P1|S],
-  take(St, N, []).
+take_all(S, S1) :-
+  list_popfront(S, _, S1).
+
+take_n([S|T], [T], N) :-
+  S = N.
+
+take_some_doit(S, N, S1) :-
+  list_pushfront(N, S, S1).
+take_some(S, S2) :-
+  list_popfront(S, E, S1),
+  E1 is E-1,
+  between(1, E1, X),
+  (
+    take_some_doit(S1, X, S2)
+  ).
+  
+/**
+ * Question 2
+ * Return true if S is a winning position
+ */
+% base case - having a single stone means losing
+win([1]) :- false.
+
