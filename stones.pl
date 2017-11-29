70f0bd2d3d8c527a66696b6355d97c26ec6f0671
JeffBobbo <james.delph@hotmail.co.uk>
2017-11-29 15:11:03 +0000

    q5 works, q4 fixed, q2 still borked

--- stones.pl
+++ stones.pl
@@ -86,6 +86,7 @@ fastwin([]).
 fastwin([1]) :- false,!.
 % recursively test again, but this time, assert iswin if it's a win
 % and don't back track
+/*
 fastwin(S) :-
   msort(S, Ss),
   forall(move(Ss, S1),
@@ -102,7 +103,58 @@ fastwin_int(S, S1) :-
     not(fastwin(S1)),
     assert(iswin(S, S1))
   ).
+*/
+fastwin([]).
+fastwin([1]) :- fail,!.
+fastwin(S) :-
+  msort(S, Ss),
+  (
+    iswin(Ss, _) ->
+    (
+      true
+    )
+    ;
+    (
+      move(Ss, S1),
+%      write('Evaluating: '),
+%      write(Ss),
+%      write(' -> '),
+%      write(S1),
+%      nl,
+      not(fastwin(S1)),
+      assert(iswin(Ss, S1))
+%       findall(Sn, move(Ss, Sn), Moves),
+%       fastwin_int(Ss, Moves)
+    )
+  ).
+
+fastwin_int(_, []) :- fail ; true.
+fastwin_int(S, [Sn|States]) :-
+  not(fastwin(Sn)),
+  assert(iswin(S, Sn)),
+  fastwin_int(S, States).
+
+buildwin(S) :-
+  msort(S, Ss),
+  (
+    iswin(Ss, _) ->
+    (
+      true
+    )
+    ;
+    (
+      move(Ss, S1),
+      not(win(S1)),
+      assert(iswin(Ss, S1))
+    )
+  ),
+  fail.
+
+win([]).
+win([1]) :- fail,!.
 win(S) :-
+  %buildwin(S);
+  %iswin(S, _).
   fastwin(S).
 
 
@@ -147,8 +199,70 @@ analyseall(N) :-
   (
     between(X, N, Y),
     (
-      S = [X|Y],
+      S = [X,Y],
       analyseall_internal(S)
     ),
     analyseall_internal([X])
   ).
+
+
+/**
+ * Question 5
+ * A play predicate, play, to play a game. One player is the user, the other is the computer
+ */
+% queries the player for input, player gives their turn by specifying
+% what the end state is. It's checked against iswin, which if fails will
+% redo on the second predicate, which just recursively calls back again to
+% reprompt the user for correct input.
+player_turn(Sin, Sout) :-
+  write('Current state: '),
+  write(Sin),
+  write('\nPossible moves: '),
+  findall(M, move(Sin, M), Moves),
+  write(Moves),
+  write('\nEnter state after move\n> '),
+  read(Sout),
+  move(Sin,Sout), !.
+player_turn(Sin, Sout) :-
+  write('Invalid play, please try again.\n'),
+  player_turn(Sin, Sout).
+
+% ust play the first thing in iswin, don't backtrack
+computer_turn(Sin, Sout) :-
+  iswin(Sin, Sout),!.
+% or the first move if there's no win, don't backtrack
+computer_turn(Sin, Sout) :-
+  move(Sin, Sout),!.
+
+game_over(S, P) :-
+  S = [],
+  P = 1,
+  write('The foolish human has been rendered a loser!\n').
+game_over(S, _) :-
+  S = [],
+  write('The mere human has beaten me, forsooth!\n').
+
+do_play(S, P) :-
+  P = 1,
+  player_turn(S, Sout),
+  (
+    game_over(Sout, 1) ->
+      fail
+    ;
+      do_play(Sout, 0)
+  ).
+do_play(S, P) :-
+  P = 0,
+  computer_turn(S, Sout),
+  (
+    game_over(Sout, 0) ->
+      fail
+    ;
+      do_play(Sout, 1)
+  ).
+
+play(S) :-
+  win(S),!,
+  do_play(S, 1).
+play(S) :-
+  do_play(S, 1).
